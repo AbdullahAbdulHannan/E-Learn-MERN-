38e705760eab43faac894bf5c7fef59447894993
AbdullahAbdulHannan <abdullahabdulhannan.ab@gmail.com>
2024-09-23 23:35:59 +0500

    commit

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
  const connectDB=async()=>{
     try {
-        await mongoose.connect("mongodb+srv://abdullahhannan450:e-platform@cluster0.x5fgi.mongodb.net/E-platform")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("connected");
         
     } catch (error) {
